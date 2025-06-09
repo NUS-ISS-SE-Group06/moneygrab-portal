e270eadb405067f137a9df7e7cee5705ce8e92de
William-Dou <xiaolongmamei@gmail.com>
2025-06-09 20:46:42 +0800

    fix unit test error

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import App from './App';
 
 
 test("renders Commission page content", () => {
- // render(<App />);
-  const heading = screen.getByText("COMMISSION SCHEME"); 
+  render(<App />);
+  const heading = screen.getByText("Account"); 
   
   expect(heading).toBeInTheDocument();
 });
