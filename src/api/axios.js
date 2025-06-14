7af0005c75cf1012a370d46b5a1c040994b27ea9
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-06-14 16:38:58 +0800

    Added the baseURL for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8688',  
+  /*
+  In your local development environment, you can use the following base URL:
+  */
+  /*baseURL: 'http://localhost:8688'*/
 
+  /*
+  In production, you can use the following base URL:
+  */
+  baseURL: 'https://r6zsml2v4c.execute-api.ap-southeast-1.amazonaws.com/PRD' // Replace with your actual API base URL
 });
 
 export default api;
