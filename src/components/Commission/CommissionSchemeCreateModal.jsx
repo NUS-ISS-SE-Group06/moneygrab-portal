e66989420e1982753184d1233e3a3c561ac619ae
Onggo Wahyudi <carnegzs@gmail.com>
2025-06-17 21:37:07 +0700

    Rebase - Conflict Refactor and adjust UI according to visily design

--- src/components/Commission/CommissionSchemeCreateModal.jsx
+++ src/components/Commission/CommissionSchemeCreateModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import api from '../api/axios';
+import api from '../../api/axios';
 import PropTypes from "prop-types";
 
 const CommissionSchemeCreateModal = ({onClose, onCreated}) => {
