081b056714e25fe2f1d385446613e5b81a614d10
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-06-19 01:43:57 +0800

    Added Cognito configs

--- src/aws/aws-exports.js
+++ src/aws/aws-exports.js
@@ -1,9 +1,26 @@
+// src/aws-exports.js
 const awsConfig = {
   Auth: {
     Cognito: {
-          region: 'ap-southeast-1',
-          userPoolId: 'ap-southeast-1_XI7LG9T1f',
-          userPoolWebClientId: '4lcfgcumiu5derbjmd2fm1io34'
+      userPoolId: 'ap-southeast-1_XI7LG9T1f',
+      userPoolClientId: '4lcfgcumiu5derbjmd2fm1io34',
+      region: 'ap-southeast-1',
+      // Add these if you have identity pool
+      // identityPoolId: 'your-identity-pool-id',
+      
+      // Optional: Add these if needed
+      loginWith: {
+        oauth: {
+          domain: 'ap-southeast-1xi7lg9t1f.auth.ap-southeast-1.amazoncognito.com',
+          scopes: ['email', 'openid', 'profile'],
+          redirectSignIn: ['http://localhost:3000/','https://d1yicjgl958p45.cloudfront.net/'],
+          redirectSignOut: ['http://localhost:3000/','https://d1yicjgl958p45.cloudfront.net/'],
+          responseType: 'code'
+        },
+        username: true,
+        email: false, // Set to true if you want email login
+        phone_number: false // Set to true if you want phone login
+      }
     }
   }
 };
