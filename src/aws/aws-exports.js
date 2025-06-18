60ccc517b7702a1c97a017e0595680e28565b638
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-06-18 19:35:06 +0800

    Added cognito config

---
+++ src/aws/aws-exports.js
@@ -0,0 +1,11 @@
+const awsConfig = {
+  Auth: {
+    Cognito: {
+          region: 'ap-southeast-1',
+          userPoolId: 'ap-southeast-1_XI7LG9T1f',
+          userPoolWebClientId: '4lcfgcumiu5derbjmd2fm1io34'
+    }
+  }
+};
+
+export default awsConfig;
