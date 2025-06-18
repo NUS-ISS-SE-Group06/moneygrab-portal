a0bbef667bcbf7960b1995a57164b8ae6cc0d1c0
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2025-06-18 19:34:55 +0800

    Updated Axios file to include ID token & Access token in local storage

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,10 +3,18 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://r6zsml2v4c.execute-api.ap-southeast-1.amazonaws.com/PRD",
   headers: {
-    "Content-Type": "application/json",
-  },
-  // Uncomment and add token if required
-  // headers: { "Authorization": `Bearer ${process.env.API_TOKEN}` },
+    "Content-Type": "application/json"
+  }
+});
+
+api.interceptors.request.use((config) => {
+  const idToken = localStorage.getItem("idToken");
+  const accessToken = localStorage.getItem("accessToken");
+
+  if (idToken) config.headers["X-ID-Token"] = idToken;
+  if (accessToken) config.headers["Authorization"] = `Bearer ${accessToken}`;
+
+  return config;
 });
 
 export default api;
