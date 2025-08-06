8fa41591454800c9219444277c2be15f459d10bd
Britta <britta@AdminNUSs-MacBook-Pro-7.local>
2025-08-06 14:48:57 +0800

    computerate test cases

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.[jt]sx?$": "babel-jest", // Transpile JS/TS/JSX/TSX
+    "^.+\\.[jt]sx?$": "babel-jest",
   },
   transformIgnorePatterns: [
     "/node_modules/(?!(axios|react|react-dom|react-router|react-router-dom)/)",
